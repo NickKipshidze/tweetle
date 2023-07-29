9297afc9df99925a0bc895e95eeff0bc3942e4af
NickKipshidze <kipshidze.nick@gmail.com>
2023-07-29 14:13:48 +0400

    Update server

--- API/main.py
+++ API/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer
+from fastapi.staticfiles import StaticFiles
+from datetime import datetime, timedelta
 from passlib.context import CryptContext
 from jose import jwt, JWTError
-from datetime import datetime, timedelta
-from pydantic import BaseModel
 from . import db
 
 app = FastAPI()
+
+app.mount("/", StaticFiles(directory="./frontend"), name="static")
 
 origins = [
     "http://127.0.0.1:5500"
@@ -36,33 +38,21 @@ def create_access_token(data: dict, expires_delta: timedelta) -> str:
     
     return encoded_jwt
 
-class UserInDB(BaseModel):
-    username: str
-    email: str
-    hashed_password: str
-
-class User(BaseModel):
-    username: str
-    password: str
-
 @app.post("/signup")
-async def signup(user: User):
-    if db.get_user(username = user):
+async def signup(user: dict):
+    if db.get_user(username = user["username"]):
         raise HTTPException(status_code=400, detail="Username already taken")
     
     db.new_user(
-        username = user.username,
-        password = user.password
+        username = user["username"],
+        password = user["password"]
     )
     
     return {"message": "User registered successfully"}
 
-@app.post("/token")
+@app.post("/login")
 async def login(user: dict):
-    if type(user) != dict:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
-    
-    if not db.get_user(username = user["username"]):
+    if type(user) != dict or not db.get_user(username = user["username"]):
         raise HTTPException(status_code=401, detail="Invalid credentials")
         
     if not db.encrypt(user["password"]) == db.get_user(username = user["username"])["password"]:
@@ -73,14 +63,16 @@ async def login(user: dict):
     
     return {"access_token": access_token, "token_type": "bearer"}
 
-@app.get("/protected_route")
-async def protected_route(token: str = Depends(oauth2_scheme)):
+@app.get("/whoami")
+async def whoami(token: str = Depends(oauth2_scheme)):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
+        payload: dict = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: any = payload.get("sub")
+        user: dict = db.get_user(username = username)
+        user.pop("password")
         if not db.get_user(username = username):
             raise HTTPException(status_code=401, detail="Invalid credentials")
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
-    return {"message": f"Hello {username}!"}
+    return user
