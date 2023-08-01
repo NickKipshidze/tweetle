8668aa24c1000af745642154e4a08ec18fd60dd7
NickKipshidze <kipshidze.nick@gmail.com>
2023-08-01 10:46:20 +0400

    Add login/signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("test", views.TestRoute.as_view(), name="test")
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("signup/", views.SignupView.as_view(), name="signup"),
+    path("test/", views.TestView.as_view())
 ]
